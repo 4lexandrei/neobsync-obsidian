b059d30794850613b048e04a60033c8d520006c1
Alexandrei <alexandreijohnvecinal@gmail.com>
2026-09-12 18:25:35 +0200

    chore: add checks to symlink script

--- scripts/symlink.mjs
+++ scripts/symlink.mjs
@@ -15,10 +15,30 @@ const resolvedVaultPath = rawVaultPath.startsWith("~")
 		)
 	: path.resolve(rawVaultPath);
 
-if (!fs.existsSync(resolvedVaultPath)) {
+const vaultStats = fs.statSync(resolvedVaultPath, {
+	throwIfNoEntry: false,
+});
+
+if (!vaultStats) {
 	console.error(`ERROR: Missing directory: ${resolvedVaultPath}`);
 	process.exit(1);
 }
+
+if (!vaultStats.isDirectory()) {
+	console.error(`ERROR: Path is not a directory: ${resolvedVaultPath}`);
+	process.exit(1);
+}
+
+const obsidianPath = path.join(resolvedVaultPath, ".obsidian");
+const obsidianStats = fs.statSync(obsidianPath, { throwIfNoEntry: false });
+
+if (!obsidianStats || !obsidianStats.isDirectory()) {
+	console.error(`ERROR: Not an obsidian vault: ${resolvedVaultPath}`);
+	process.exit(1);
+}
+
+const pluginPath = path.join(obsidianPath, "plugins", "neobsync-obsidian");
+fs.mkdirSync(pluginPath, { recursive: true });
 
 const filesToLink = ["main.js", "manifest.json"];
 const projectRoot = process.cwd();
@@ -27,17 +47,27 @@ console.log(`Symlinking files from ${projectRoot} to ${resolvedVaultPath}`);
 
 filesToLink.forEach((file) => {
 	const sourceFile = path.join(projectRoot, file);
-	const targetFile = path.join(resolvedVaultPath, file);
+	const targetFile = path.join(pluginPath, file);
 
 	try {
-		if (
-			fs.existsSync(targetFile) &&
-			fs.lstatSync(targetFile).isSymbolicLink()
-		) {
-			console.log(`${file} is already symlinked`);
-			return;
+		const stats = fs.lstatSync(targetFile, { throwIfNoEntry: false });
+
+		if (stats) {
+			if (stats.isSymbolicLink()) {
+				console.log(
+					`${path.basename(targetFile)} already exists; replacing it`,
+				);
+				fs.unlinkSync(targetFile);
+			} else if (stats.isFile()) {
+				console.log(
+					`${path.basename(targetFile)} already exists; replacing it`,
+				);
+				fs.rmSync(targetFile);
+			}
 		}
+
 		fs.symlinkSync(sourceFile, targetFile, "file");
+
 		console.log(` ✅ Linked: ${file} -> "${targetFile}"`);
 	} catch {
 		console.log(`ERROR: Couldn't create symlink for ${file}`);
